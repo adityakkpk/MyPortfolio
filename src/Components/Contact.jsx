0a9ba70ad8451c870ff3594c4544845f59a2baf4
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-05-29 10:56:03 +0530

    Finished Animating

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,41 +1,65 @@
-import React from 'react'
-import {CONTACT} from '../constants'
-import {FaLinkedin} from 'react-icons/fa'
-import {FaGithub} from 'react-icons/fa'
-import {FaSquareXTwitter} from 'react-icons/fa6'
+import React from "react";
+import { CONTACT } from "../constants";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div id='contact' className='border-b border-neutral-800 pb-4 pt-4'>
-        <h2 className='my-20 text-center text-4xl'>
-            Get in 
-            <span className='text-neutral-500'> Touch</span>
-        </h2>
-        <div className="text-center tracking-tighter">
-            <p className="my-4">
-                {CONTACT.address}
-            </p>
-            <p className='my-4'>
-                {CONTACT.phoneNo}
-            </p>
-            <a href="#" className="border-b">
-                {CONTACT.email}
-            </a>
-        </div>
+    <div id="contact" className="border-b border-neutral-800 pb-4 pt-4">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Get in
+        <span className="text-neutral-500"> Touch</span>
+      </motion.h2>
+      <div className="text-center tracking-tighter">
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.address}
+        </motion.p>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
+        <a
+          href="#"
+          className="border-b"
+        >
+          {CONTACT.email}
+        </a>
+      </div>
 
-        <div className="flex flex-wrap items-center justify-center text-2xl gap-4 px-3 py-10">
-            <a href="https://www.linkedin.com/in/adityakumarkushwaha">
-                <FaLinkedin />
-            </a>
-            <a href="https://github.com/adityakkpk">
-                <FaGithub />
-            </a>
-            <a href="https://x.com/akkpk933">
-                <FaSquareXTwitter />
-            </a>
-        </div>
+      <motion.div
+        whileInView={{ opacity: 1, Y: 0 }}
+        initial={{ opacity: 0, Y: 100 }}
+        transition={{ duration: 0.5 }}
+        className="flex flex-wrap items-center justify-center text-2xl gap-4 px-3 py-10"
+      >
+        <a href="https://www.linkedin.com/in/adityakumarkushwaha">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/adityakkpk">
+          <FaGithub />
+        </a>
+        <a href="https://x.com/akkpk933">
+          <FaSquareXTwitter />
+        </a>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
