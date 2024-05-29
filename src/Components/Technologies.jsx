8f6745f22557f598f913337d2b79127fb6501e52
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-05-29 11:09:22 +0530

    Added id to technologies

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -22,7 +22,7 @@ const iconVariants = (duration) => ({
 
 const Technologies = () => {
   return (
-    <div className="border-b border-neutral-800 pb-24 pt-4">
+    <div id="technologies" className="border-b border-neutral-800 pb-24 pt-4">
       <motion.h2
         whileInView={{ opacity: 1, y: 0 }}
         initial={{opacity: 0, y:-100}}
